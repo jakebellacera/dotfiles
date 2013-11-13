b35f005f68dfe5082d388893f50658f5e9c1bc52
Jake Bellacera <jakeb@eleveninc.com>
2013-11-13 00:26:48 -0800

    Ghetto

--- home/.js/bank.simple.com.js
+++ home/.js/bank.simple.com.js
@@ -4,20 +4,18 @@
  * To toggle the Safe To Spend banner, press "e".
  */
 
-var safeToSpend = document.querySelectorAll('#safe-to-spend > .row')[0],
-	isHidden = true;
-
-safeToSpend.style.opacity = '0';
-safeToSpend.style.transition = 'opacity 200ms linear';
+var safeToSpend,
+	isHidden;
 
 document.addEventListener('keydown', function (e) {
+	safeToSpend = document.querySelectorAll('#safe-to-spend > .row')[0];
+	isHidden = safeToSpend.style.opacity === '0';
+
 	if (e.keyCode === 69) { // the 'e' key
 		if (isHidden) {
 			safeToSpend.style.opacity = '1';
 		} else {
 			safeToSpend.style.opacity = '0';
 		}
-
-		isHidden = !isHidden; // invert
 	}
 }, false);
